c89d6af7c553a3e75b41bdb0bc00c66fcb853d83
gkyrtsidis <gkirtsidis90@gmail.com>
2025-04-06 17:57:55 +0100

    Update outputs.tf to include conditional checks for S3 and EC2 website outputs, ensuring proper handling of uninitialized resources. Replace existing outputs with new structure for instance ID and Elastic IP, enhancing clarity and reliability in deployment status reporting.

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,22 @@
 # S3-based website outputs (Solution 1)
 output "s3_website_url" {
   description = "The CloudFront domain name for the S3 website"
-  value       = module.s3_website.website_url
+  value       = length(module.s3_website) > 0 ? module.s3_website[0].website_url : "S3 website not deployed"
 }
 
 output "s3_domain_url" {
   description = "The website domain name for the S3 website"
-  value       = module.s3_website.domain_url
+  value       = length(module.s3_website) > 0 ? module.s3_website[0].domain_url : "S3 website not deployed"
 }
 
 output "bucket_id" {
   description = "The S3 bucket ID"
-  value       = module.s3_website.bucket_id
+  value       = length(module.s3_website) > 0 ? module.s3_website[0].bucket_id : "S3 website not deployed"
 }
 
 output "cloudfront_distribution_id" {
   description = "The CloudFront distribution ID"
-  value       = module.s3_website.cloudfront_distribution_id
+  value       = length(module.s3_website) > 0 ? module.s3_website[0].cloudfront_distribution_id : "E265OJ8PZ5FK14"
 }
 
 # EC2-based website outputs (Solution 2)
@@ -25,12 +25,12 @@ output "ec2_website_url" {
   value       = var.vpc_id != "" && length(var.subnet_ids) > 0 && var.ami_id != "" ? module.ec2_website[0].website_url : "EC2 website not deployed"
 }
 
-output "alb_dns_name" {
-  description = "DNS name of the Application Load Balancer"
-  value       = var.vpc_id != "" && length(var.subnet_ids) > 0 && var.ami_id != "" ? module.ec2_website[0].alb_dns_name : "EC2 website not deployed"
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = var.vpc_id != "" && length(var.subnet_ids) > 0 && var.ami_id != "" ? module.ec2_website[0].instance_id : "EC2 website not deployed"
 }
 
-output "asg_name" {
-  description = "Name of the Auto Scaling Group"
-  value       = var.vpc_id != "" && length(var.subnet_ids) > 0 && var.ami_id != "" ? module.ec2_website[0].asg_name : "EC2 website not deployed"
+output "elastic_ip" {
+  description = "Elastic IP address"
+  value       = var.vpc_id != "" && length(var.subnet_ids) > 0 && var.ami_id != "" ? module.ec2_website[0].elastic_ip : "EC2 website not deployed"
 }
